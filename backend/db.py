089e2447c9f138d79789c61328f3bd33166485d6
Jeremy Fisher <jeremy.adams.fisher@gmail.com>
2022-04-09 11:52:43 -0400

    Add in-memory option

--- backend/db.py
+++ backend/db.py
@@ -3,16 +3,21 @@ import os
 from google.cloud.sql.connector import connector
 from sqlmodel import Session, create_engine
 
-engine = create_engine(
-    "postgresql+pg8000://",
-    creator=lambda: connector.connect(
-        "story-circle-ai:us-east1:yakul",
-        "pg8000",
-        user="story-circle-app-sa@story-circle-ai.iam",
-        db="faboo",
-        enable_iam_auth=True,
-    ),
-)
+if os.environ.get("IN_MEMORY_DB") == "1":
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+else:
+    engine = create_engine(
+        "postgresql+pg8000://",
+        creator=lambda: connector.connect(
+            "story-circle-ai:us-east1:yakul",
+            "pg8000",
+            user="story-circle-app-sa@story-circle-ai.iam",
+            db="faboo",
+            enable_iam_auth=True,
+        ),
+    )
 
 
 if os.environ.get("IN_MEMORY_DB") == "1":
