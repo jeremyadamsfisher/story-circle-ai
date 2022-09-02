5c44ecd7834daeb35d408704cdd07607302d1552
Jeremy Fisher <jeremyf@andrew.cmu.edu>
2022-09-02 13:02:19 -0700

    Show whose turn it is

--- frontend/components/StoryEditor.tsx
+++ frontend/components/StoryEditor.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Avatar,
   Box,
   Center,
   Spinner,
@@ -8,12 +9,18 @@ import {
   AlertTitle,
   AlertDescription,
   useColorModeValue,
+  HStack,
+  Text,
 } from "@chakra-ui/react";
 import { useStory } from "../lib/story";
 import StoryNewLineField from "../components/StoryNewLineField";
 import { hashString } from "../lib/utils";
+import { BeatLoader } from "react-spinners";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/auth";
 
 export default () => {
+  const [user] = useAuthState(auth);
   const { story, error } = useStory();
   const outline = {
     shadow: "xs",
@@ -36,6 +43,11 @@ export default () => {
         <AlertDescription>{error.toString()}</AlertDescription>
       </Alert>
     );
+
+  const isPlayerTurn = user
+    ? story.whose_turn_is_it.name === user.email
+    : story.whose_turn_is_it.single_player === true;
+
   return (
     <Box {...outline} textAlign={"center"} background={greyBg}>
       {story.segments.map((segment, i: number) => {
@@ -56,7 +68,29 @@ export default () => {
           </>
         );
       })}
-      <StoryNewLineField />
+      {isPlayerTurn ? (
+        <StoryNewLineField />
+      ) : story.whose_turn_is_it.ai_player ? (
+        <span>
+          <BeatLoader size={5} />
+        </span>
+      ) : (
+        <Center>
+          <Box
+            mt={10}
+            p={2}
+            w={"auto"}
+            borderWidth={1}
+            rounded={"md"}
+            bg={greyBg}
+          >
+            <HStack>
+              <Text>Waiting for</Text>
+              <Avatar size={"xs"} name={story.whose_turn_is_it.name} />
+            </HStack>
+          </Box>
+        </Center>
+      )}
     </Box>
   );
 };
