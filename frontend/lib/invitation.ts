f9586664db7e7732773da442a11859b4181eb8a9
Jeremy Fisher <jeremyf@andrew.cmu.edu>
2022-09-02 13:52:45 -0700

    Memoize invitation callback with api client

--- frontend/lib/invitation.ts
+++ frontend/lib/invitation.ts
@@ -8,16 +8,20 @@ type InvitationRead = schemas["InvitationRead"];
 
 export const useSendInvitationCallback = () => {
   const apiClient = useApiClient();
-  return useCallback(async (storyUuid: string, inviteeEmail: string) => {
-    const payload: InvitationNew = {
-      story_uuid: storyUuid,
-      invitee_email: inviteeEmail,
-    };
-    const invitation: InvitationRead = await apiClient
-      .post("invitations/send", { json: payload })
-      .json();
-    return invitation;
-  }, []);
+  return useCallback(
+    async (storyUuid: string, inviteeEmail: string) => {
+      const payload: InvitationNew = {
+        story_uuid: storyUuid,
+        invitee_email: inviteeEmail,
+      };
+      console.log({ payload });
+      const invitation: InvitationRead = await apiClient
+        .post("invitations/send", { json: payload })
+        .json();
+      return invitation;
+    },
+    [apiClient]
+  );
 };
 
 export const useRespondToInvitationCallback = () => {
