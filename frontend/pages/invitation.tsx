136b0cbac3637b9cc7252133faa002d0b888955c
Jeremy Fisher <jeremyf@andrew.cmu.edu>
2022-09-07 22:19:27 -0700

    Throw error when using the wrong email
    TODO: sign out as well

--- frontend/pages/invitation.tsx
+++ frontend/pages/invitation.tsx
@@ -4,6 +4,7 @@ import {
   AlertIcon,
   AlertTitle,
   AlertDescription,
+  useToast,
 } from "@chakra-ui/react";
 import { LogInButton } from "../components/LogInOutButtons";
 import { Text, Center, VStack } from "@chakra-ui/react";
@@ -15,16 +16,31 @@ import { auth } from "../lib/auth";
 export default () => {
   const router = useRouter();
   const respondToInvitation = useRespondToInvitationCallback();
+  const toast = useToast();
   const [user] = useAuthState(auth);
 
   useEffect(() => {
     if (user && router.isReady) {
-      respondToInvitation(router.query!.id as string).then((invitation) => {
-        router.push({
-          pathname: "/story",
-          query: { id: invitation.story.story_uuid },
+      respondToInvitation(router.query!.id as string)
+        .then((invitation) => {
+          router.push({
+            pathname: "/story",
+            query: { id: invitation.story.story_uuid },
+          });
+        })
+        .catch((error) => {
+          const { response } = error;
+          if (response.statusCode === 422) {
+            toast({
+              title: "Email does not match",
+              description:
+                "Please log in with the email to which the invitation was sent",
+              status: "error",
+              duration: 9000,
+              isClosable: true,
+            });
+          }
         });
-      });
     }
   }, [user, router.isReady, router.query?.id]);
 
